8b9712568becd289bfd9ff45cda4d55d09d7fcc5
Steven Cantrell <steven.cantrell@gtri.gatech.edu>
2021-12-10 15:12:01 +0000

    day1 part 2

--- day01/Program.cs
+++ day01/Program.cs
@@ -1,14 +1,43 @@
 ﻿using AOC;
 
-var measurements = InputParser.ParseListOfInts("../inputs/day01.txt");
-int measurementIncreases = 0;
-int lastMeasurement = measurements.First();
-foreach (var measurement in measurements.Skip(1))
+static void PartOne(List<int> measurements)
 {
-    if (measurement > lastMeasurement)
+    int measurementIncreases = 0;
+    int lastMeasurement = measurements.First();
+    foreach (var measurement in measurements.Skip(1))
     {
-        measurementIncreases++;
+        if (measurement > lastMeasurement)
+        {
+            measurementIncreases++;
+        }
+        lastMeasurement = measurement;
     }
-    lastMeasurement = measurement;
+    Console.WriteLine($"Part one measurement increases: {measurementIncreases}");
+    // Answer is 1602
 }
-Console.WriteLine($"Measurement increases: {measurementIncreases}");
+
+static void PartTwo(List<int> measurements)
+{
+    int measurementIncreases = 0;
+    int windowLength = 3;
+    int lastMeasurementAccum = measurements[0] + measurements[1] + measurements[2];
+    for (int windowStartIndex = 1; windowStartIndex < measurements.Count - windowLength + 1; windowStartIndex++)
+    {
+        int measurementAccum = 0;
+        for (int i = windowStartIndex; i < windowStartIndex + windowLength; i++)
+        {
+            measurementAccum += measurements[i];
+        }
+        if (measurementAccum > lastMeasurementAccum)
+        {
+            measurementIncreases++;
+        }
+        lastMeasurementAccum = measurementAccum;
+    }
+    Console.WriteLine($"Part two measurement increases: {measurementIncreases}");
+    // Answer is 1633
+}
+
+var measurements = InputParser.ParseListOfInts("../inputs/day01.txt");
+PartOne(measurements);
+PartTwo(measurements);
