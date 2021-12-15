5efe819022f3948e61a4f78502f2ec85595f2ca7
Steven Cantrell <steven.cantrell@gtri.gatech.edu>
2021-12-15 01:42:18 +0000

    day 11 init

---
+++ day11/Program.cs
@@ -0,0 +1,13 @@
+﻿static void PartOne(string filepath)
+{
+    // Answer is 
+}
+
+static void PartTwo(string filepath)
+{
+    // Answer is 
+}
+
+var filepath = "../inputs/day11_example.txt";
+PartOne(filepath);
+PartTwo(filepath);
